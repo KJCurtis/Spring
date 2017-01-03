d139d1737dced5206c73808419a91f6fcb197469
Kenneth Curtis <Kenneth.D.Curtis@outlook.com>
2017-01-03 13:35:09 -0600

    removed un used content from profile page

--- src/main/java/com/codeup/adlister/controllers/adIndividUpdateServlet.java
+++ src/main/java/com/codeup/adlister/controllers/adIndividUpdateServlet.java
@@ -1,6 +1,8 @@
 package com.codeup.adlister.controllers;
 
 import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.models.Ad;
+import com.codeup.adlister.models.User;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,15 +13,26 @@ import java.io.IOException;
 
 
 
-@WebServlet(name = "controllers.AdsIndexServlet", urlPatterns = "/ad_individ/ad/update")
+@WebServlet(name = "controllers.AdsIndexServlet", urlPatterns = "/ads/update")
 public class adIndividUpdateServlet extends HttpServlet {
-    protected void doGet(
-            HttpServletRequest request,
-            HttpServletResponse response
-    ) throws ServletException, IOException {
-        String id = request.getParameter("id");
-        request.setAttribute("ads", DaoFactory.getAdsDao().specAd(id));
-        request.getRequestDispatcher("/WEB-INF/ads/adUpdate.jsp").forward(request, response);
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (request.getSession().getAttribute("user") == null) {
+            response.sendRedirect("/login");
+            return;
+        }
+        request.getRequestDispatcher("/WEB-INF/ads/adUpdate.jsp")
+                .forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        User user = (User) request.getSession().getAttribute("user");
+        Ad ad = new Ad(
+                user.getId(),
+                request.getParameter("title"),
+                request.getParameter("description")
+        );
+        DaoFactory.getAdsDao().insert(ad);
+        response.sendRedirect("/ads");
     }
 }
 
