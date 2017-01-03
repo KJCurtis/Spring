da0c3e05a2c40b138c7cb97f0fe18f41e4241f2a
Kenneth Curtis <Kenneth.D.Curtis@outlook.com>
2017-01-03 11:44:43 -0600

    getting the ad content displayed in the text areas

--- src/main/java/com/codeup/adlister/controllers/adIndividUpdateServlet.java
+++ src/main/java/com/codeup/adlister/controllers/adIndividUpdateServlet.java
@@ -1,5 +1,7 @@
 package com.codeup.adlister.controllers;
 
+import com.codeup.adlister.dao.DaoFactory;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -15,7 +17,8 @@ public class adIndividUpdateServlet extends HttpServlet {
             HttpServletRequest request,
             HttpServletResponse response
     ) throws ServletException, IOException {
-
+        String id = request.getParameter("id");
+        request.setAttribute("ads", DaoFactory.getAdsDao().specAd(id));
         request.getRequestDispatcher("/WEB-INF/ads/adUpdate.jsp").forward(request, response);
     }
 }
